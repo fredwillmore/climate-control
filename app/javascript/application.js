84cb2d1c1e853f7c7df0792b81de8e8f8a5f4e1a
Fred Willmore <fredwillmore@gmail.com>
2024-06-16 13:41:56 -0700

    trying to add stimulus js package

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-// import "stimulus.min.js"
+import "stimulus.min.js"
 // import "stimulus-loading.js"
 // import "controllers"
 // import "@hotwired/turbo-rails"
