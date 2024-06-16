6602460f6281074485e19adc614a5ab21c36cb28
Fred Willmore <fredwillmore@gmail.com>
2024-06-16 12:57:46 -0700

    removed references to previous rails installation

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module ClimateControl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -30,6 +30,6 @@ module ClimateControl
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
   end
 end
