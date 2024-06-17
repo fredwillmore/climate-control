211a69462c17a5e4958ce7cb6a20c9100c25b530
Fred Willmore <fredwillmore@gmail.com>
2024-06-16 19:14:14 -0700

    got hotwire working!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-import "stimulus.min.js"
-// import "stimulus-loading.js"
-// import "controllers"
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
+import "controllers"
