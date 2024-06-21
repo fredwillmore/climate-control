123d1759e32d9cfe2c4639cb192222a8a1c0c731
Fred Willmore <fredwillmore@gmail.com>
2024-06-21 14:41:24 -0700

    addressed deprecation warnings in tests

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module ClimateControl
   class Application < Rails::Application
+    config.load_defaults 7.1
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.2
 
