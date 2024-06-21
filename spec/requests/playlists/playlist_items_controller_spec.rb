2746c4afc1af04aea595a93108d14a22d6ed89bb
Fred Willmore <fredwillmore@gmail.com>
2024-06-21 14:25:01 -0700

    cleaned up instance variables in test

--- spec/requests/playlists/playlist_items_controller_spec.rb
+++ spec/requests/playlists/playlist_items_controller_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 require 'support/factory_bot'
 
 describe Playlists::PlaylistItemsController do
-  before do
-    @artist = create :artist, { name: 'Test Artist' }
-    @track =  create :track, { name: 'Hello World', artist: @artist } 
-    @track2 =  create :track, { name: 'Second Track', artist: @artist } 
-    @track3 =  create :track, { name: 'Third Track', artist: @artist } 
-    @playlist =  create :playlist, { date: '2000-01-01' } 
-    @playlist_item =  create :playlist_item, { track: @track, playlist: @playlist, position: 1 } 
-    @playlist_item2 =  create :playlist_item, { track: @track2, playlist: @playlist, position: 2 } 
-    @playlist_item3 =  create :playlist_item, { track: @track3, playlist: @playlist, position: 3 } 
-  end
+  let!(:artist) { create :artist, name: 'Test Artist' }
+  let!(:track) { create :track, name: 'Hello World', artist: artist }
+  let!(:track2) { create :track, name: 'Second Track', artist: artist }
+  let!(:track3) { create :track, name: 'Third Track', artist: artist }
+  let!(:playlist) { create :playlist, date: '2000-01-01' }
+  let!(:playlist_item) { create :playlist_item, track: track, playlist: playlist, position: 1 }
+  let!(:playlist_item2) { create :playlist_item, track: track2, playlist: playlist, position: 2 }
+  let!(:playlist_item3) { create :playlist_item, track: track3, playlist: playlist, position: 3 }
 
   describe "GET #index" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items", params: { format: :html }
+      get "/playlists/#{playlist.id}/playlist_items", params: { format: :html }
     end
 
     it "returns http success" do
@@ -30,7 +28,7 @@ describe Playlists::PlaylistItemsController do
 
   describe "GET #show" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items/#{@playlist_item.id}", params: { format: :html }
+      get "/playlists/#{playlist.id}/playlist_items/#{playlist_item.id}", params: { format: :html }
     end
 
     it "returns http success" do
@@ -44,7 +42,7 @@ describe Playlists::PlaylistItemsController do
 
   describe "GET #first" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items/first"
+      get "/playlists/#{playlist.id}/playlist_items/first"
     end
 
     it "returns http success" do
@@ -55,7 +53,7 @@ describe Playlists::PlaylistItemsController do
       record = nil
       expect { record = JSON.parse(response.body) }.not_to raise_exception
       expect(record.slice('id', 'position')).to match({
-       'id' => @playlist_item.id,
+       'id' => playlist_item.id,
        'position' => 1
       })
     end
@@ -63,7 +61,7 @@ describe Playlists::PlaylistItemsController do
 
   describe "GET #last" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items/last"
+      get "/playlists/#{playlist.id}/playlist_items/last"
     end
 
     it "returns http success" do
@@ -74,7 +72,7 @@ describe Playlists::PlaylistItemsController do
       record = nil
       expect { record = JSON.parse(response.body) }.not_to raise_exception
       expect(record.slice('id', 'position')).to match({
-       'id' => @playlist_item3.id,
+       'id' => playlist_item3.id,
        'position' => 3
       })
     end
@@ -82,7 +80,7 @@ describe Playlists::PlaylistItemsController do
 
   describe "GET #next" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items/#{@playlist_item.id}/next"
+      get "/playlists/#{playlist.id}/playlist_items/#{playlist_item.id}/next"
     end
 
     it "returns http success" do
@@ -93,14 +91,14 @@ describe Playlists::PlaylistItemsController do
       record = nil
       expect { record = JSON.parse(response.body) }.not_to raise_exception
       expect(record.slice('id', 'position')).to match({
-       'id' => @playlist_item2.id,
+       'id' => playlist_item2.id,
        'position' => 2
       })
     end
     
     it "returns null for next on last item" do
       record = 'not nil'
-      get "/playlists/#{@playlist.id}/playlist_items/#{@playlist_item3.id}/next"
+      get "/playlists/#{playlist.id}/playlist_items/#{playlist_item3.id}/next"
       expect { record = JSON.parse(response.body) }.not_to raise_exception
       expect(record).to be_nil
     end
@@ -108,7 +106,7 @@ describe Playlists::PlaylistItemsController do
 
   describe "GET #previous" do
     before do
-      get "/playlists/#{@playlist.id}/playlist_items/#{@playlist_item.id}/previous"
+      get "/playlists/#{playlist.id}/playlist_items/#{playlist_item.id}/previous"
     end
 
     it "returns http success" do
@@ -117,10 +115,10 @@ describe Playlists::PlaylistItemsController do
 
     it "response with JSON body containing artist" do
       record = nil
-      get "/playlists/#{@playlist.id}/playlist_items/#{@playlist_item3.id}/previous"
+      get "/playlists/#{playlist.id}/playlist_items/#{playlist_item3.id}/previous"
       expect { record = JSON.parse(response.body) }.not_to raise_exception
       expect(record.slice('id', 'position')).to match({
-       'id' => @playlist_item2.id,
+       'id' => playlist_item2.id,
        'position' => 2
       })
     end
