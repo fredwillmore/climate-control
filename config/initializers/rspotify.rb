e9e6411ca24e0761cf96b06fa56b035f335b43d8
Fred Willmore <fredwillmore@gmail.com>
2024-08-09 22:30:36 -0700

    use environment variables in spotify connection

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,4 +1,16 @@
+case Rails::env
+when "development", "test"
+  spotify_client_id = Rails.application.credentials['spotify_client_id']
+  spotify_client_secret = Rails.application.credentials['spotify_client_secret']
+when "production"
+  spotify_client_id = ENV['SPOTIFY_CLIENT_ID']
+  spotify_client_secret = ENV['SPOTIFY_CLIENT_SECRET']
+default
+  spotify_client_id = 'FIXME'
+  spotify_client_secret = 'FIXME'
+end
+
 RSpotify.authenticate(
-  Rails.application.credentials['spotify_client_id'],
-  Rails.application.credentials['spotify_client_secret']
+  spotify_client_id,
+  spotify_client_secret
 )
